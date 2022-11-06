20cae0d348071bf2402bdaa68077bf6090c77b86
André Vieira <andrevieirass@gmail.com>
2022-11-06 17:27:32 -0300

    Add files from Day 6

---
+++ Day6/day-6-c1-functions.py
@@ -0,0 +1,35 @@
+# Python Built-n Functions
+# For more built-in functions, access the link below
+# https://docs.python.org/3/library/functions.html
+print("Hello")  # Print function
+
+num_char = len("Hello")  # Len function
+
+print(num_char)
+
+int_to_str = str(12345)
+
+print(int_to_str, type(int_to_str))  # Str function
+
+print("\n##########\n")
+
+# Defining Function
+# def my_function():
+#   Do something
+def my_function():
+    print("Hello")
+    print("Bye")
+
+my_function()
+
+# Reeborg Challenge
+"""
+def turn_around():
+    turn_left()
+    turn_left()
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+"""
