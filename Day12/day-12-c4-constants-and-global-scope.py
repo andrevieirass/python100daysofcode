acfd8cc71b65ee65306346699d703055b12d4ccd
André Vieira <andrevieirass@gmail.com>
2022-11-21 19:55:40 -0300

    Add files from Day 12

---
+++ Day12/day-12-c4-constants-and-global-scope.py
@@ -0,0 +1,10 @@
+# Constant and Global Scope
+# If you don't want to change the variable value and transform it into a 
+# constant, turn it into all uppercase
+PI = 3.14159
+URL = "https://www.google.com"
+
+def calc():
+    print(PI + 2)
+
+calc()
