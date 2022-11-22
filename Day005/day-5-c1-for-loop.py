40eb374665c40967562175f342dd5c1b2f6544ed
André Vieira <andrevieirass@gmail.com>
2022-11-22 18:59:33 -0300

    Add files from Day 5

---
+++ Day005/day-5-c1-for-loop.py
@@ -0,0 +1,10 @@
+# For Loop - List
+# for item in list_of_items:
+    # print(item)
+fruits = ["Apple", "Peach", "Pear"]
+
+for fruit in fruits:
+    print(fruit)
+    print(fruit + " Pie")
+
+print(fruits)
