ee1c4f0bc58ab1ddff0da898b732f8d7c8f432a2
André Vieira <andrevieirass@gmail.com>
2022-11-22 19:27:22 -0300

    Add files from Day 13

--- Day013/day-13-c1-debugging.py
+++ Day013/day-13-c1-debugging.py
@@ -52,14 +52,14 @@
 
 ##########
 
-# Use a Debugger
-def mutate(a_list):
-    b_list = []
+# # Use a Debugger
+# def mutate(a_list):
+#     b_list = []
 
-    for item in a_list:
-        new_item = item * 2
-        b_list.append(new_item)
+#     for item in a_list:
+#         new_item = item * 2
+#         b_list.append(new_item)
     
-    print(b_list)
+#     print(b_list)
 
-mutate([1,2,3,5,8,13])
+# mutate([1,2,3,5,8,13])
