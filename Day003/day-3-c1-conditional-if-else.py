b395109ee438daa161806a7c16268df275f710ae
André Vieira <andrevieirass@gmail.com>
2022-11-22 18:57:20 -0300

    Add files from Day 3

---
+++ Day003/day-3-c1-conditional-if-else.py
@@ -0,0 +1,22 @@
+print("Welcome to the rollercoaster!")
+
+height = int(input("What is your height in cm? "))
+
+if height > 120:  # Greater than
+    print("You can ride the rollercoaster!")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
+
+#####
+
+if height >= 120:  # Greater than or equal to
+    print("You can ride the rollercoaster!")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
+
+#####
+
+if height == 120:  # Equal to
+    print("Your height is equal to 120cm")
+else:
+    print("Sorry, your height is not equal to 120cm.")
